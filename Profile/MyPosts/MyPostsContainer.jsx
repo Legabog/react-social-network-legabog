08c285b40e39981635c0e24e5ec8a92a8768f6cd
legabog <probringer@mail.ru>
2020-04-19 01:24:48 +0300

    Add files via upload

---
+++ Profile/MyPosts/MyPostsContainer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  updatePostActionCreator,
+  addPostActionCreator
+} from "../../../redux/profile-reducer";
+import MyPosts from "./MyPosts";
+import { connect } from "react-redux";
+
+let mapStateToProps = state => {
+  return {
+    PostsData: state.profileReducer.PostsData,
+    newPostText: state.profileReducer.newPostText
+  }
+}
+
+let mapDispatchToProps = dispatch => {
+  return {
+    updateNewPostText: text => {
+      dispatch(updatePostActionCreator(text))
+    },
+    addPost: () => {
+      dispatch(addPostActionCreator());
+    }
+  }
+}
+
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
+
+export default MyPostsContainer;
---
+++ Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from "./Post.module.css";
+
+const Post = props => {
+  return (
+    <div className={s.item}>
+      <img src="https://s01.geekpic.net/di-JX2ZZP.jpeg" />
+      {props.message}
+      <div>
+        <span>like count: {props.likes}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
