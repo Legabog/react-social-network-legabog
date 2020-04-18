416ca035aa3dbd081d2172d8459821c7cbb658d0
legabog <probringer@mail.ru>
2020-04-19 01:24:29 +0300

    Add files via upload

--- react-practice-v2-legabog/src/components/Users/Users.jsx
+++ react-practice-v2-legabog/src/components/Users/Users.jsx
@@ -1,64 +1,76 @@
 import React from "react";
-import classes from "./Users.module.css";
-import * as axios from "axios";
 import UserPhoto from "../../assets/images/user.png";
+import { makeStyles } from "@material-ui/core/styles";
+import Pagination from "@material-ui/lab/Pagination";
+import classes from "./Users.module.css";
+import { NavLink } from "react-router-dom";
+import { updatePostActionCreator } from "../../redux/profile-reducer";
 
-class Users extends React.Component {
-  componentDidMount() {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response => {
-        this.props.setusers(response.data.items);
-      });
+let Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  render() {
-    return (
-      <div>
-        {this.props.users.map(e => (
-          <div key={e.id}>
-            <span>
-              <div>
-                <img
-                  src={e.photos.small != null ? e.photos.small : UserPhoto}
-                  className={classes.userphoto}
-                />
-              </div>
-              <div>
-                {e.followed ? (
-                  <button
-                    onClick={() => {
-                      this.props.unfollow(e.id);
-                    }}
-                  >
-                    Follow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      this.props.follow(e.id);
-                    }}
-                  >
-                    Unfollow
-                  </button>
-                )}
-              </div>
-            </span>
-            <span>
-              <span>
-                <div>{e.name}</div>
-                <div>{e.status}</div>
-              </span>
-              <span>
-                <div>{"e.location.city"}</div>
-                <div>{"e.location.country"}</div>
-              </span>
-            </span>
-          </div>
-        ))}
+  return (
+    <div>
+      <div className={classes.root}>
+        <Pagination
+          color="primary"
+          count={pagesCount}
+          onChange={(e, page) => {
+            props.onPageChanged(page);
+          }}
+        />
       </div>
-    );
-  }
-}
+
+      {props.users.map((e) => (
+        <div key={e.id}>
+          <span>
+            <div>
+              <NavLink to={'/profile/' + e.id}>
+              <img
+                src={e.photos.small != null ? e.photos.small : UserPhoto}
+                className={classes.userphoto}
+              />
+              </NavLink>
+            </div>
+            <div>
+              {e.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(e.id);
+                  }}
+                >
+                  Follow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(e.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{e.name}</div>
+              <div>{e.status}</div>
+            </span>
+            <span>
+              <div>{"e.location.city"}</div>
+              <div>{"e.location.country"}</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Users;
--- react-practice-v2-legabog/src/components/Users/UsersContainer.jsx
+++ react-practice-v2-legabog/src/components/Users/UsersContainer.jsx
@@ -1,26 +1,100 @@
 import React from "react";
-import Users from "./Users";
 import { connect } from "react-redux";
-import { followAC, unfollowAC, setUsersAC } from "../../redux/users-reducer";
+import {
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  setTotalUsersCount,
+  toggleIsFetching,
+} from "../../redux/users-reducer";
+import * as axios from "axios";
+import Users from "./Users";
+import Preloader from "../common/Preloader/Preloader.jsx";
 
-const mapStateToProps = state => {
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    toggleIsFetching(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    this.props.toggleIsFetching(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.isFetching ? <Preloader /> : null}
+        <Users
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          onPageChanged={this.onPageChanged}
+          users={this.props.users}
+          unfollow={this.props.unfollow}
+          follow={this.props.follow}
+        />
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
   return {
-    users: state.usersReducer.users
+    users: state.usersReducer.users,
+    pageSize: state.usersReducer.pageSize,
+    totalUsersCount: state.usersReducer.totalUsersCount,
+    currentPage: state.usersReducer.currentPage,
+    isFetching: state.usersReducer.isFetching,
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    follow: userId => {
-      dispatch(followAC(userId));
-    },
-    unfollow: userId => {
-      dispatch(unfollowAC(userId));
-    },
-    setusers: users => {
-      dispatch(setUsersAC(users));
-    }
-  };
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     follow: (userId) => {
+//       dispatch(followAC(userId));
+//     },
+//     unfollow: (userId) => {
+//       dispatch(unfollowAC(userId));
+//     },
+//     setusers: (users) => {
+//       dispatch(setUsersAC(users));
+//     },
+//     setCurrentPage: (pageNumber) => {
+//       dispatch(setCurrentPageAC(pageNumber));
+//     },
+//     setTotalUsersCount: (users) => {
+//       dispatch(setTotalUsersCountAC(users));
+//     },
+//     toggleIsFetching: (isFetching) => {
+//       dispatch(toggleIsFetchingAC(isFetching));
+//     },
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, {
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  setTotalUsersCount,
+  toggleIsFetching,
+})(UsersContainer);
