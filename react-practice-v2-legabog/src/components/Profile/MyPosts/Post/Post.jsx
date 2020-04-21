548efa0cb906c846449dbf34deb6b5a66794c517
legabog <probringer@mail.ru>
2020-04-21 20:50:58 +0300

    Add files via upload

--- react-practice-v2-legabog/src/components/Profile/MyPosts/Post/Post.jsx
+++ react-practice-v2-legabog/src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ import s from "./Post.module.css";
 const Post = props => {
   return (
     <div className={s.item}>
-      <img src="https://s01.geekpic.net/di-JX2ZZP.jpeg" />
+      <img src="https://s01.geekpic.net/di-JX2ZZP.jpeg" alt="description" />
       {props.message}
       <div>
         <span>like count: {props.likes}</span>
