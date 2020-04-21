284fc7371ef82088c5d1f0c700beef836fa30c14
legabog <probringer@mail.ru>
2020-04-21 20:51:16 +0300

    Add files via upload

---
+++ Users/UsersContainer.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { connect } from "react-redux";
+import {
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  setTotalUsersCount,
+  toggleIsFetching,
+  toggleFollowingProgress
+
+} from "../../redux/users-reducer";
+import Users from "./Users";
+import Preloader from "../common/Preloader/Preloader.jsx";
+import { userAPI } from "../../api/api";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    toggleIsFetching(true);
+
+    userAPI.getUsers(this.props.currentPage, this.props.pageSize).then((response) => {
+      this.props.toggleIsFetching(false);
+      this.props.setUsers(response.items);
+      this.props.setTotalUsersCount(response.totalCount);
+    });
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    this.props.toggleIsFetching(true);
+    userAPI.getUsers(pageNumber, this.props.pageSize).then((response) => {
+      this.props.toggleIsFetching(false);
+      this.props.setUsers(response.items);
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.isFetching ? <Preloader /> : null}
+        <Users
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          onPageChanged={this.onPageChanged}
+          users={this.props.users}
+          unfollow={this.props.unfollow}
+          follow={this.props.follow}
+          followingInProgress={this.props.followingInProgress}
+          toggleFollowingProgress={this.props.toggleFollowingProgress}
+        />
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.usersReducer.users,
+    pageSize: state.usersReducer.pageSize,
+    totalUsersCount: state.usersReducer.totalUsersCount,
+    currentPage: state.usersReducer.currentPage,
+    isFetching: state.usersReducer.isFetching,
+    followingInProgress: state.usersReducer.followingInProgress
+  };
+};
+
+
+export default connect(mapStateToProps, {
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  setTotalUsersCount,
+  toggleIsFetching,
+  toggleFollowingProgress
+})(UsersContainer);
