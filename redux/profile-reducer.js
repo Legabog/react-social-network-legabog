0415eddc94e9ecd35c4067e644a0523cb6f1ed97
legabog <probringer@mail.ru>
2020-04-22 22:14:07 +0300

    Add files via upload

---
+++ redux/profile-reducer.js
@@ -0,0 +1,69 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+const SET_USER_PROFILE = "SET_USER_PROFILE";
+
+
+let initialState = {
+  PostsData: [
+    { id: 1, message: "Hi, how are you?", likes: 20 },
+    { id: 2, message: "It's my first post", likes: 30 },
+    { id: 3, message: "q", likes: 2 },
+    { id: 4, message: "w", likes: 12 },
+    { id: 5, message: "e", likes: 23 }
+  ],
+  newPostText: "legabozhestvo",
+  profile: null
+};
+
+const profileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST: {
+      let newPost = {
+        id: 6,
+        message: state.newPostText,
+        likes: 0
+      };
+      let stateCopy = {
+        ...state,
+        PostsData: [...state.PostsData, newPost],
+      }
+      return stateCopy;
+    }
+    case UPDATE_NEW_POST_TEXT: {
+      return {
+        ...state,
+        newPostText: action.newText
+      }
+    }
+    case SET_USER_PROFILE: {
+      return {
+        ...state,
+        profile: action.profile
+      }
+    }
+    default:
+      return state;
+  }
+};
+
+export const addPostActionCreator = () => {
+  return {
+    type: ADD_POST
+  };
+};
+
+export const updatePostActionCreator = text => {
+  return {
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text
+  };
+};
+
+export const setUserProfile = (profile) => {
+  return {
+    type: SET_USER_PROFILE,
+    profile
+  }
+};
+
+export default profileReducer;
