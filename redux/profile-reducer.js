79d730c2f1a7d5716f787b4661255e12ca3a6625
legabog <probringer@mail.ru>
2020-05-06 20:15:32 +0300

    Add files via upload

--- redux/profile-reducer.js
+++ redux/profile-reducer.js
@@ -1,7 +1,10 @@
+import { userAPI } from "../api/api";
+
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
-
+const SET_PROFILE_STATUS = "SET_PROFILE_STATUS";
+const CHANGE_STATUS_HANDLER = "CHANGE_STATUS_HANDLER"
 
 let initialState = {
   PostsData: [
@@ -9,10 +12,11 @@ let initialState = {
     { id: 2, message: "It's my first post", likes: 30 },
     { id: 3, message: "q", likes: 2 },
     { id: 4, message: "w", likes: 12 },
-    { id: 5, message: "e", likes: 23 }
+    { id: 5, message: "e", likes: 23 },
   ],
   newPostText: "legabozhestvo",
-  profile: null
+  profile: null,
+  profileStatus: "Change status",
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -21,24 +25,38 @@ const profileReducer = (state = initialState, action) => {
       let newPost = {
         id: 6,
         message: state.newPostText,
-        likes: 0
+        likes: 0,
       };
       let stateCopy = {
         ...state,
         PostsData: [...state.PostsData, newPost],
-      }
+      };
       return stateCopy;
     }
     case UPDATE_NEW_POST_TEXT: {
       return {
         ...state,
-        newPostText: action.newText
-      }
+        newPostText: action.newText,
+      };
     }
     case SET_USER_PROFILE: {
       return {
         ...state,
-        profile: action.profile
+        profile: action.profile,
+      };
+    }
+
+    case SET_PROFILE_STATUS: {
+      return {
+        ...state,
+        profileStatus: action.status ? action.status : "Change status",
+      };
+    }
+
+    case CHANGE_STATUS_HANDLER: {
+      return {
+        ...state,
+        profileStatus: action.status
       }
     }
     default:
@@ -48,22 +66,60 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => {
   return {
-    type: ADD_POST
+    type: ADD_POST,
   };
 };
 
-export const updatePostActionCreator = text => {
+export const updatePostActionCreator = (text) => {
   return {
     type: UPDATE_NEW_POST_TEXT,
-    newText: text
+    newText: text,
   };
 };
 
 export const setUserProfile = (profile) => {
   return {
     type: SET_USER_PROFILE,
-    profile
+    profile,
+  };
+};
+
+export const setProfileStatus = (status) => {
+  return {
+    type: SET_PROFILE_STATUS,
+    status,
+  };
+};
+
+export const changeStatusHandler = (status) => {
+  return {
+    type: CHANGE_STATUS_HANDLER,
+    status
   }
+}
+
+export const getProfile = (userId) => {
+  return (dispatch) =>
+    userAPI.getProfile(userId).then((response) => {
+      dispatch(setUserProfile(response));
+    });
+};
+
+export const getProfileStatus = (userId) => {
+  return (dispatch) =>
+    userAPI.getProfileStatus(userId).then((response) => {
+      dispatch(setProfileStatus(response));
+    });
+};
+
+export const updateProfileStatus = (status) => {
+  return (dispatch) => {
+    userAPI.updateProfileStatus(status).then((response) => {
+      if (response.resulCode === 0) {
+        dispatch(setProfileStatus(status));
+      }
+    });
+  };
 };
 
 export default profileReducer;
